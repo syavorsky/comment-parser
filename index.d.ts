86bda557c3917b7d47794c26b925b2e5c1546322
Javier Ciberman Mora <jhm.ciberman@gmail.com>
2019-01-04 05:18:51 -0300

    Update typings

--- index.d.ts
+++ index.d.ts
@@ -2,26 +2,28 @@
 // Project: comment-parser
 // Definitions by: Javier "Ciberman" Mora <https://github.com/jhm-ciberman/>
 
-interface Comment {
-  tags: Tag[];
-  line: number;
-  description: string;
-  source: string;
-}
-interface Tag {
-  tag: string;
-  name: string;
-  optional: boolean;
-  type: string;
-  description: string;
-  line: number;
-  source: string;
-}
-interface Options {
-  parsers?: [(str: string, data: any) => { source: string, data: any }];
-  dotted_names?: boolean;
+declare namespace CommentParser {
+  interface Comment {
+    tags: Tag[];
+    line: number;
+    description: string;
+    source: string;
+  }
+  interface Tag {
+    tag: string;
+    name: string;
+    optional: boolean;
+    type: string;
+    description: string;
+    line: number;
+    source: string;
+  }
+  interface Options {
+    parsers?: [(str: string, data: any) => { source: string, data: any }];
+    dotted_names?: boolean;
+  }
 }
 
-declare function parse(str: string, opts?: Options): [Comment];
+declare function parse(str: string, opts?: CommentParser.Options): [CommentParser.Comment];
 
 export = parse;
