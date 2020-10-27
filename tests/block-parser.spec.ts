1cb9b4f6197fb5d9eb9c0ba54bd31d7b5ddfe8b9
Sergiy Yavorsky <sergiy@yavorsky.me>
2020-10-26 23:31:47 -0700

    add more block-parser tests

--- tests/block-parser.spec.ts
+++ tests/block-parser.spec.ts
@@ -2,85 +2,99 @@ import getParser from '../src/block-parser'
 import { Line } from '../src/types'
 import { seedTokens } from '../src/util'
 
-const block: Line[] = [{
-  number: 1,
-  source: '    /**',
-  tokens: seedTokens({ start: '    ', delimiter: '/**', postDelimiter: '', description: '', end: '' })
-}, {
-  number: 2,
-  source: '     * description 0',
-  tokens: seedTokens({ start: '     ', delimiter: '*', postDelimiter: ' ', description: 'description 0', end: '' })
-}, {
-  number: 3,
-  source: '     *',
-  tokens: seedTokens({ start: '     ', delimiter: '*', postDelimiter: '', description: '', end: '' })
-}, {
-  number: 4,
-  source: '     * description 1',
-  tokens: seedTokens({ start: '     ', delimiter: '*', postDelimiter: ' ', description: 'description 1', end: '' })
-}, {
-  number: 5,
-  source: '     *',
-  tokens: seedTokens({ start: '     ', delimiter: '*', postDelimiter: '', description: '', end: '' })
-}, {
-  number: 6,
-  source: '     * @param {string} value value description 0',
-  tokens: seedTokens({ start: '     ', delimiter: '*', postDelimiter: ' ', description: '@param {string} value value description 0', end: '' })
-}, {
-  number: 7,
-  source: '    ```',
-  tokens: seedTokens({ start: '    ', delimiter: '', postDelimiter: '', description: '```', end: '' })
-}, {
-  number: 8,
-  source: '    @sample code',
-  tokens: seedTokens({ start: '    ', delimiter: '', postDelimiter: '', description: '@sample code', end: '' })
-}, {
-  number: 9,
-  source: '    ```',
-  tokens: seedTokens({ start: '    ', delimiter: '', postDelimiter: '', description: '```', end: '' })
-}, {
-  number: 10,
-  source: '    * value description 1',
-  tokens: seedTokens({ start: '    ', delimiter: '*', postDelimiter: ' ', description: 'value description 1', end: '' })
-}, {
-  number: 11,
-  source: '    */',
-  tokens: seedTokens({ start: '    ', delimiter: '', postDelimiter: '', description: '', end: '*/' })
-}]
+let source: Line[] 
 
-test('groups lines', () => {
+beforeEach(() => {
+  source = [{
+    number: 1,
+    source: '    /**',
+    tokens: seedTokens({ start: '    ', delimiter: '/**', postDelimiter: '', description: '', end: '' })
+  }, {
+    number: 2,
+    source: '     * description 0',
+    tokens: seedTokens({ start: '     ', delimiter: '*', postDelimiter: ' ', description: 'description 0', end: '' })
+  }, {
+    number: 3,
+    source: '     *',
+    tokens: seedTokens({ start: '     ', delimiter: '*', postDelimiter: '', description: '', end: '' })
+  }, {
+    number: 4,
+    source: '     * description 1',
+    tokens: seedTokens({ start: '     ', delimiter: '*', postDelimiter: ' ', description: 'description 1', end: '' })
+  }, {
+    number: 5,
+    source: '     *',
+    tokens: seedTokens({ start: '     ', delimiter: '*', postDelimiter: '', description: '', end: '' })
+  }, {
+    number: 6,
+    source: '     * @param {string} value value description 0',
+    tokens: seedTokens({ start: '     ', delimiter: '*', postDelimiter: ' ', description: '@param {string} value value description 0', end: '' })
+  }, {
+    number: 7,
+    source: '    ```',
+    tokens: seedTokens({ start: '    ', delimiter: '', postDelimiter: '', description: '```', end: '' })
+  }, {
+    number: 8,
+    source: '    @sample code',
+    tokens: seedTokens({ start: '    ', delimiter: '', postDelimiter: '', description: '@sample code', end: '' })
+  }, {
+    number: 9,
+    source: '    ```',
+    tokens: seedTokens({ start: '    ', delimiter: '', postDelimiter: '', description: '```', end: '' })
+  }, {
+    number: 10,
+    source: '    * value description 1',
+    tokens: seedTokens({ start: '    ', delimiter: '*', postDelimiter: ' ', description: 'value description 1', end: '' })
+  }, {
+    number: 11,
+    source: '    */',
+    tokens: seedTokens({ start: '    ', delimiter: '', postDelimiter: '', description: '', end: '*/' })
+  }]
+})
+
+test('standard fences', () => {
   const parser = getParser()
-  const groups:Line[][] = parser(block)
+  const groups:Line[][] = parser(source)
 
   expect(groups.length).toBe(2)
   expect(groups).toEqual([
-    block.slice(0, 5),
-    block.slice(5)
+    source.slice(0, 5),
+    source.slice(5)
   ])    
 })
 
-// test('join: "compact"', () => {
-//   const parser = getParser({ join: 'compact' })
-//   const items = parser(block)
+test('custom fence', () => {
+  source = source.map(line => {
+    line.tokens.description = line.tokens.description.replace('```', '###')
+    return line
+  })
 
-//   expect(items).toBe([{
-//     text: 'description 0 description 1',
-//     source: block.slice(0, 5)
-//   }, {
-//     text: '@param {string} value value description 0 ``` @sample code ``` value description 1',
-//     source: block.slice(5)
-//   }])
-// })
+  const parser = getParser({fence: '###'})
+  const groups:Line[][] = parser(source)
 
-// test('join: "multiline"', () => {
-//   const parser = getParser({ join: 'multiline' })
-//   const items = parser(block)
+  expect(groups.length).toBe(2)
+  expect(groups).toEqual([
+    source.slice(0, 5),
+    source.slice(5)
+  ])    
+})
 
-//   expect(items).toBe([{
-//     text: 'description 0\n\ndescription 1\n',
-//     source: block.slice(0, 5)
-//   }, {
-//     text: '@param {string} value value description 0\n    ```\n    @sample code\n    ```\nvalue description 1',
-//     source: block.slice(5)
-//   }])
-// })
+test('fence function', () => {
+  source = source.map(line => {
+    line.tokens.description = line.tokens.description.replace('```', '###')
+    return line
+  })
+
+  function isFenced (source: string) { 
+    return source.split('###').length % 2 === 0 
+  }
+  
+  const parser = getParser({fence: isFenced})
+  const groups:Line[][] = parser(source)
+
+  expect(groups.length).toBe(2)
+  expect(groups).toEqual([
+    source.slice(0, 5),
+    source.slice(5)
+  ])    
+})
