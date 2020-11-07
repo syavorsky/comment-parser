83e4a114857ae5e430056654dfcceff342cfd06e
Sergiy Yavorsky <sergiy@yavorsky.me>
2020-11-07 12:37:16 -0800

    util tests

--- src/util.ts
+++ src/util.ts
@@ -1,12 +1,12 @@
 import { Block, Tokens, Spec } from './types';
 
 export function isSpace(source: string): boolean {
-  return /^\s$/.test(source);
+  return /^\s+$/.test(source);
 }
 
 export function splitSpace(source: string): [string, string] {
-  const matches = source.match(/^\s*/);
-  return matches === null
+  const matches = source.match(/^\s+/);
+  return matches == null
     ? ['', source]
     : [source.slice(0, matches[0].length), source.slice(matches[0].length)];
 }
