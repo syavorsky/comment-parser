1a63654854fd709d62bb1ca1913a561a924c6914
Sergiy Yavorsky <sergiy@yavorsky.me>
2020-11-10 00:01:13 -0800

    examples

--- examples/parse-string/index.js
+++ examples/parse-string/index.js
@@ -1,11 +1,11 @@
-const parse = require('../../lib').default()
+const {parse} = require('../../lib/')
 
 const source = `
 /**
  * Description may go 
  * over few lines followed by @tags
- * @param name {string} name parameter
- * @param value {any} value of any type
+ * @param {string} name the name parameter
+ * @param {any} value the value parameter
  */`
 
- console.log(parse(source))
+ console.log(JSON.stringify(parse(source), null, 2))
