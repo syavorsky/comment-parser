ec14c940ccc61a2ddba966beb056686b3d78cc87
Sergiy Yavorsky <sergiy@yavorsky.me>
2021-01-07 02:38:07 -0800

    introduce inspect() helper

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import getParser, { Options as ParserOptions } from './parser/index';
 import getStringifier from './stringifier/index';
-import { Block } from './primitives';
 
 export function parse(source: string, options: Partial<ParserOptions> = {}) {
   return getParser(options)(source);
 }
 
 export const stringify = getStringifier();
+export { default as inspect } from './stringifier/inspect';
 
 export * as transforms from './transforms/index';
