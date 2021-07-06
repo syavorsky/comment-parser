8f1cdaf673dd33303d741bb9aac58f0b77f38649
Sergiy Yavorsky <sergiy@yavorsky.me>
2021-07-06 05:41:46 +0300

    add e2e test for issue #129

---
+++ tests/e2e/issue-129.spec.js
@@ -0,0 +1,111 @@
+const { parse, inspect } = require('../../lib');
+
+const tokens = {
+  start: '',
+  delimiter: '',
+  postDelimiter: '',
+  tag: '',
+  postTag: '',
+  type: '',
+  postType: '',
+  name: '',
+  postName: '',
+  description: '',
+  end: '',
+};
+
+test('carriage returns', () => {
+  const parsed = parse(`
+    /**
+     * description\r
+     * @param0 {param-type}\r
+     * @param1 {param-type} paramName param description\r
+     */`);
+
+  const source = [
+    {
+      number: 1,
+      source: '    /**',
+      tokens: {
+        ...tokens,
+        start: '    ',
+        delimiter: '/**',
+      },
+    },
+    {
+      number: 2,
+      source: '     * description\r',
+      tokens: {
+        ...tokens,
+        start: '     ',
+        delimiter: '*',
+        postDelimiter: ' ',
+        description: 'description\r',
+      },
+    },
+    {
+      number: 3,
+      source: '     * @param0 {param-type}\r',
+      tokens: {
+        ...tokens,
+        start: '     ',
+        delimiter: '*',
+        postDelimiter: ' ',
+        tag: '@param0',
+        postTag: ' ',
+        type: '{param-type}',
+        postType: '\r',
+      },
+    },
+    {
+      number: 4,
+      source: '     * @param1 {param-type} paramName param description\r',
+      tokens: {
+        ...tokens,
+        start: '     ',
+        delimiter: '*',
+        postDelimiter: ' ',
+        tag: '@param1',
+        postTag: ' ',
+        type: '{param-type}',
+        postType: ' ',
+        name: 'paramName',
+        postName: ' ',
+        description: 'param description\r',
+      },
+    },
+    {
+      number: 5,
+      source: '     */',
+      tokens: {
+        ...tokens,
+        start: '     ',
+        end: '*/',
+      },
+    },
+  ];
+
+  expect(parsed[0]).toMatchObject({
+    description: 'description',
+    problems: [],
+    source,
+    tags: [
+      {
+        tag: 'param0',
+        type: 'param-type',
+        name: '',
+        optional: false,
+        description: '',
+        source: [source[2]],
+      },
+      {
+        tag: 'param1',
+        type: 'param-type',
+        name: 'paramName',
+        optional: false,
+        description: 'param description',
+        source: [source[3], source[4]],
+      },
+    ],
+  });
+});
