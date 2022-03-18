1e4a246b570e8ae6ab7dc4d68b4add2851d162c8
Dominik Bošnjak <47727604+dddominikk@users.noreply.github.com>
2022-03-18 05:36:01 +0100

    minor comment typo

--- src/parser/tokenizers/tag.ts
+++ src/parser/tokenizers/tag.ts
@@ -2,7 +2,7 @@ import { Spec } from '../../primitives';
 import { Tokenizer } from './index';
 
 /**
- * Splits the `@prefix` from remaining `Spec.lines[].token.descrioption` into the `tag` token,
+ * Splits the `@prefix` from remaining `Spec.lines[].token.description` into the `tag` token,
  * and populates `spec.tag`
  */
 export default function tagTokenizer(): Tokenizer {
